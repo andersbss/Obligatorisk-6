9460e1568d1c666bc4366098be47b76f2d396c76
andersbss <43789401+andersbss@users.noreply.github.com>
2020-09-28 11:50:41 +0200

    nav comp

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = ({ header1, header2 }) => {
+  <header>
+    <nav>
+      <a>{header1}</a>
+      <a>{header2}</a>
+    </nav>
+  </header>;
+};
+
+export default Navbar;
