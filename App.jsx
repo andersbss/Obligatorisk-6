7058172c60a5d58db3a3541025606389ce0f2178
andersbss <43789401+andersbss@users.noreply.github.com>
2020-09-28 13:30:53 +0200

    CompletedList

--- App.jsx
+++ App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Navbar from './src/components/Navbar';
 import TodoButton from './src/components/TodoButton';
 import TodoCardList from './src/components/TodoCardList';
+import CompletedList from './src/components/CompletedList';
 import TodoCard from './src/components/TodoCard';
+import CompletedListItem from './src/components/CompletedListItem';
 
 const App = () => (
   <>
@@ -10,6 +12,7 @@ const App = () => (
     <main>
       <TodoButton />
       <TodoCardList />
+      <CompletedList />
     </main>
     <aside className="popup" id="popup" />
   </>
--- src/components/CompletedList.jsx
+++ src/components/CompletedList.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import Title from './Title';
 
 const CompletedList = () => (
-  <table id="completedTodosTable">
-    <tr id="headers">
-      <th id="titleHeader">Title</th>
-      <th id="authorHeader">Author</th>
-      <th id="descHeader">Description</th>
-      <th id="dateHeader">Completed date</th>
-    </tr>
-  </table>
+  <section>
+    <Title title="Completed todos" />
+    <form id="filterCheckbox">
+      <input type="checkbox" />
+      <label>Filter by date</label>
+    </form>
+    <table id="completedTodosTable">
+      <tr id="headers">
+        <th id="titleHeader">Title</th>
+        <th id="authorHeader">Author</th>
+        <th id="descHeader">Description</th>
+        <th id="dateHeader">Completed date</th>
+      </tr>
+    </table>
+  </section>
 );
+
+export default CompletedList;
