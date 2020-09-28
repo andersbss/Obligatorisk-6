d11caa56c547b0627691d34991727ee7385c8696
andersbss <43789401+andersbss@users.noreply.github.com>
2020-09-28 13:06:59 +0200

    Title and CompletedList

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './src/components/Navbar';
 import TodoButton from './src/components/TodoButton';
+import Title from './src/components/Title';
 
 const App = () => (
   <>
     <Navbar header1="HIOF" header2="User user" />
-    <TodoButton />
+    <main>
+      <TodoButton />
+    </main>
+    <aside className="popup" id="popup" />
   </>
 );
 
---
+++ src/components/CompletedListItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CompletedListItem = ({ title, author, description, date }) => (
+  <tr className="completedTodo">
+    <td>{title}</td>
+    <td>{author}</td>
+    <td>{description}</td>
+    <td>{date}</td>
+  </tr>
+);
