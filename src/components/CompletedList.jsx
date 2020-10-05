c1d79311263dbb5b9d1e86b540db6ddf45e74ec5
andersbss <43789401+andersbss@users.noreply.github.com>
2020-10-05 20:05:17 +0200

    refactor

--- src/components/CompletedList.jsx
+++ src/components/CompletedList.jsx
@@ -2,50 +2,47 @@ import React from 'react';
 import Title from './Title';
 import CompletedListItem from './CompletedListItem';
 
-const CompletedList = ({ completedTodos, handleSearch, search }) => {
-  const handleChange = (e) => {
-    handleSearch(e);
-  };
+const CompletedList = ({ completedTodos, handleSearch, search }) => (
+  // const handleChange = (e) => {
+  //  handleSearch(e);
+  // };
 
-  return (
-    <section id="completedTodos">
-      <Title title="Completed todos" />
-      <form id="filterCheckbox">
-        <input type="checkbox" />
-        <label>Filter by date</label>
-      </form>
-      <form id="search">
-        <input
-          className="searchField"
-          type="text"
-          id="search"
-          placeholder="Search"
-          onChange={handleChange}
-          value={search}
-        />
-      </form>
-      <table id="completedTodosTable">
-        <thead>
-          <tr id="headers">
-            <th id="titleHeader">Title</th>
-            <th id="authorHeader">Author</th>
-            <th id="descHeader">Description</th>
-            <th id="dateHeader">Completed date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {completedTodos().map((todo) => (
-            <CompletedListItem
-              key={todo.id}
-              title={todo.title}
-              author={todo.author}
-              description={todo.description}
-            />
-          ))}
-        </tbody>
-      </table>
-    </section>
-  );
-};
-
+  <section id="completedTodos">
+    <Title title="Completed todos" />
+    <form id="filterCheckbox">
+      <input type="checkbox" />
+      <label>Filter by date</label>
+    </form>
+    <form id="search">
+      <input
+        className="searchField"
+        type="text"
+        id="search"
+        placeholder="Search"
+        onChange={handleSearch}
+        value={search}
+      />
+    </form>
+    <table id="completedTodosTable">
+      <thead>
+        <tr id="headers">
+          <th id="titleHeader">Title</th>
+          <th id="authorHeader">Author</th>
+          <th id="descHeader">Description</th>
+          <th id="dateHeader">Completed date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {completedTodos().map((todo) => (
+          <CompletedListItem
+            key={todo.id}
+            title={todo.title}
+            author={todo.author}
+            description={todo.description}
+          />
+        ))}
+      </tbody>
+    </table>
+  </section>
+);
 export default CompletedList;
